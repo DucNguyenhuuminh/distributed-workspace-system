3cd0438535c43c916edba41ffc6533ca41e9fb12
DucNguyenhuuminh <nguyenhuuminhduc2832005@gmail.com>
2026-06-01 10:59:10 +0700

    fix bug noti 7

--- services/noti-service/src/consumers/index.consumer.js
+++ services/noti-service/src/consumers/index.consumer.js
@@ -1,10 +1,11 @@
 const {createWorker, QUEUES} = require('shared');
 const {notificationProcessor} = require('../handlers/noti.handler');
+const {workspaceProcessor} = require('../handlers/workspace.handler');
 
 function startAllConsumers() {
     createWorker(QUEUES.WORKSPACE, workspaceProcessor, {concurrency: 5});
     console.log('[Consumer] workspace-queue');
-    
+
     createWorker(QUEUES.NOTIFICATION, notificationProcessor, {concurrency: 10});
     console.log('[Consumer] notification-queue');
 
